0171743dba60fc55141c3ea802f8a2730491c4e0
ExtraExaByte <55938181+ExtraExaByte@users.noreply.github.com>
2022-02-27 14:40:08 -0800

    new

--- motor.py
+++ motor.py
@@ -1,114 +1,82 @@
-from __future__ import division
-import time
-# Import the PCA9685 module.
-import Adafruit_PCA9685
-import RPi.GPIO as GPIO
-# Initialise the PCA9685 using the default address (0x40).
-pwm = Adafruit_PCA9685.PCA9685()
+import RPi.GPIO as GPIO          
+from time import sleep
 
-# Alternatively specify a different address and/or bus:
-#pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
+in1 = 24
+in2 = 23
+en = 25
+temp1=1
 
-move_speed = 2000  # Max pulse length out of 4096
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in1,GPIO.OUT)
+GPIO.setup(in2,GPIO.OUT)
+GPIO.setup(en,GPIO.OUT)
+GPIO.output(in1,GPIO.LOW)
+GPIO.output(in2,GPIO.LOW)
+p=GPIO.PWM(en,1000)
+p.start(25)
+print("\n")
+print("The default speed & direction of motor is LOW & Forward.....")
+print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
+print("\n")    
 
-# Set frequency to 60hz, good for servos.
-pwm.set_pwm_freq(60)
-GPIO.setmode(GPIO.BCM) # GPIO number  in BCM mode
-GPIO.setwarnings(False)
-#define L298N(Model-Pi motor drive board) GPIO pins
-IN1 = 23  #Left motor direction pin
-IN2 = 24  #Left motor direction pin
-IN3 = 27  #Right motor direction pin
-IN4 = 22  #Right motor direction pin
-ENA = 0  #Left motor speed PCA9685 port 0
-ENB = 1  #Right motor speed PCA9685 port 1
+while(1):
 
-# Define motor control  pins as output
-GPIO.setup(IN1, GPIO.OUT)   
-GPIO.setup(IN2, GPIO.OUT) 
-GPIO.setup(IN3, GPIO.OUT)   
-GPIO.setup(IN4, GPIO.OUT) 
-
-def changespeed(speed):
-	pwm.set_pwm(ENA, 0, speed)
-	pwm.set_pwm(ENB, 0, speed)
-
-def stopcar():
-	GPIO.output(IN1, GPIO.LOW)
-	GPIO.output(IN2, GPIO.LOW)
-	GPIO.output(IN3, GPIO.LOW)
-	GPIO.output(IN4, GPIO.LOW)
-	changespeed(0)
+    x=raw_input()
+    
+    if x=='r':
+        print("run")
+        if(temp1==1):
+         GPIO.output(in1,GPIO.HIGH)
+         GPIO.output(in2,GPIO.LOW)
+         print("forward")
+         x='z'
+        else:
+         GPIO.output(in1,GPIO.LOW)
+         GPIO.output(in2,GPIO.HIGH)
+         print("backward")
+         x='z'
 
 
-def backward():
-	GPIO.output(IN1, GPIO.HIGH)
-	GPIO.output(IN2, GPIO.LOW)
-	GPIO.output(IN3, GPIO.HIGH)
-	GPIO.output(IN4, GPIO.LOW)
-	changespeed(move_speed)
- 
-	#following two lines can be removed if you want car make continuous movement without pause
-	#time.sleep(0.25)  
-	#stopcar()
-	
-def forward():
-	GPIO.output(IN2, GPIO.HIGH)
-	GPIO.output(IN1, GPIO.LOW)
-	GPIO.output(IN4, GPIO.HIGH)
-	GPIO.output(IN3, GPIO.LOW)
-	changespeed(move_speed)
-	#following two lines can be removed if you want car make continuous movement without pause
-	#time.sleep(0.25)  
-	#stopcar()
-	
-def turnRight():
-	GPIO.output(IN1, GPIO.LOW)
-	GPIO.output(IN2, GPIO.HIGH)
-	GPIO.output(IN3, GPIO.HIGH)
-	GPIO.output(IN4, GPIO.LOW)
-	changespeed(move_speed)
-	#following two lines can be removed if you want car make continuous movement without pause
-	#time.sleep(0.25)  
-	#stopcar()
-	
-def turnLeft():
-	GPIO.output(IN1, GPIO.HIGH)
-	GPIO.output(IN2, GPIO.LOW)
-	GPIO.output(IN3, GPIO.LOW)
-	GPIO.output(IN4, GPIO.HIGH)
-	changespeed(move_speed)	
-	#following two lines can be removed if you want car make continuous movement without pause
-	#time.sleep(0.25)  
-	#stopcar()
-	
-forward()
-time.sleep(1)  
-stopcar()
-time.sleep(0.25)
+    elif x=='s':
+        print("stop")
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.LOW)
+        x='z'
 
-backward()
-time.sleep(1)  
-stopcar()
-time.sleep(0.25) 
+    elif x=='f':
+        print("forward")
+        GPIO.output(in1,GPIO.HIGH)
+        GPIO.output(in2,GPIO.LOW)
+        temp1=1
+        x='z'
 
-turnLeft()
-time.sleep(1)  
-stopcar()
-time.sleep(0.25)
-	
-turnRight()
-time.sleep(1)  
-stopcar()
-time.sleep(0.25)
+    elif x=='b':
+        print("backward")
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.HIGH)
+        temp1=0
+        x='z'
 
- 
+    elif x=='l':
+        print("low")
+        p.ChangeDutyCycle(25)
+        x='z'
 
+    elif x=='m':
+        print("medium")
+        p.ChangeDutyCycle(50)
+        x='z'
 
-
-print('Moving servo on channel 0, press Ctrl-C to quit...')
-#while True:
-# Move servo on channel O between extremes.
-
-time.sleep(2)
-pwm.set_pwm(15, 0, 0)
+    elif x=='h':
+        print("high")
+        p.ChangeDutyCycle(75)
+        x='z'
+     
+    
+    elif x=='e':
+        GPIO.cleanup()
+        break
+    
+    else:
+        print("<<<  wrong data  >>>")
+        print("please enter the defined data to continue.....")
